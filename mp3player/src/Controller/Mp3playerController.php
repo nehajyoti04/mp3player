1afd82f2089987ae50ae3cf5e78aca2f6ea58b73
JYOTI BOHRA <nehajyoti04@gmail.com>
2016-08-24 15:20:18 +0530

    fixing errors on mp3 list page

--- mp3player/src/Controller/Mp3playerController.php
+++ mp3player/src/Controller/Mp3playerController.php
@@ -1,9 +1,10 @@
 <?php
 
-
 namespace Drupal\mp3player\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Url;
+use Drupal\Core\Link;
 
 class Mp3playerController extends ControllerBase {
 
@@ -24,17 +25,20 @@ class Mp3playerController extends ControllerBase {
     $rows = array();
     $players = $this->mp3player_players();
     foreach ($players as $pid => $player) {
+
+      // print '<pre>'; print_r("player"); print '</pre>';
+      // print '<pre>'; print_r($player); print '</pre>';
       $row = array();
       $row[] = $player['name'];
-      $links = array();
-      $links[] = l(t('Edit'), 'admin/config/media/mp3player/player/'. $player['pid']);
+      $url = Url::fromRoute('mp3player.edit_player', array('pid' => $pid));
+      $row[] = \Drupal::l(t('Edit'), $url);
       if($pid > 1) {
-        $links[] = l(t('Delete'), 'admin/config/media/mp3player/player/'. $player['pid'] .'/delete');
+        $url = Url::fromRoute('mp3player.delete_player', array('pid' => $pid));
+        $row[] = \Drupal::l(t('Delete'), $url);
       }
-      $row[] = implode('&nbsp;&nbsp;&nbsp;&nbsp;', $links);
       $rows[] = $row;
     }
-    $output = array('type' => 'markup', 'header' => $header, 'rows' => $rows);
+    $output = array('#type' => 'table', '#header' => $header, '#rows' => $rows);
     return $output;
 
   }
@@ -43,17 +47,83 @@ class Mp3playerController extends ControllerBase {
   /**
    * Return list of all available players along with settings.
    */
-  function mp3player_players($pid = NULL) {
-    $players = array();
-//    $result = db_select('mp3player_players', 'm')
-//      ->fields('m')
-//      ->execute();
-//    while($player = $result->fetchAssoc()) {
-//      $players[$player['pid']] = $player;
-//    }
+//   public function mp3player_players($pid = NULL) {
+//     $players = array();
+//    // $result = db_select('mp3player_players', 'm')
+//    //   ->fields('m')
+//    //   ->execute();
+
+
+
+
+// $query = \Drupal::database()->select('mp3player_players', 'nfd');
+// // $query->fields('nfd', ['pid', 'name']);
+// $query->fields('nfd');
+// // $query->condition('nfd.type', 'vegetable');
+// // $query->range(0, 1);
+// $result = $query->execute()->fetchAll();
+// // print '<pre>'; print_r($result); print '</pre>';
+
+// foreach ($result as $key => $player) {
+// // print '<pre>';  print_r($player);
+//  // $x = (array)$player;
+//  $x = get_object_vars($player);
+// // print '<pre>'; print_r($x); print '</pre>';
+// // $players[$player->pid] = $player;
+// $y[$player->pid] = $x;
+// }
+
+// // print '<pre>'; print_r($y); print '</pre>';
+
+// // while($player = $result->fetchAssoc()) {
+//     // dpm("player");
+//     // dpm($player);
+// //      $players[$player['pid']] = $player;
+//    // }
+
+
+
+//      // print $result;
+//    // while($player = $result->fetchAssoc()) {
+//     // dpm("player");
+//     // dpm($player);
+//      // $players[$player['pid']] = $player;
+//    // }
 //    if($pid && array_key_exists($pid, $players)) {
 //      return $players[$pid];
 //    }
+//    return $y;
+
+//     return $players;
+//   }
+
+// }
+
+/**
+   * Return list of all available players along with settings.
+   */
+  public static function mp3player_players($pid = NULL) {
+    $players = array();
+    // $player = array();
+   $result = db_select('mp3player_players', 'm')
+     ->fields('m')
+     ->execute();
+     // dpm("results");
+     // dpm($result);
+   while($player = $result->fetchAssoc()) {
+    // dpm("player");
+    // dpm($player);
+
+     $players[$player['pid']] = $player;
+     // dpm("players");
+     // dpm($players);
+   }
+
+   // $player = array('pid' => 1, 'name' => 'hello', 'type' => 'hello type');
+   // $players[$player['pid']] = $player;
+   // if($pid && array_key_exists($pid, $players)) {
+   //   return $players[$pid];
+   // }
     return $players;
   }
 
